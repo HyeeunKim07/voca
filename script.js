ffb055c7e0ceabd275cfbffc41c1693aa64c5300
HyeeunKim07 <studying.hyeeun07@gmail.com>
2023-12-10 23:23:07 +0900

    change colors

---
+++ script.js
@@ -0,0 +1,35 @@
+var currentCardIndex = 0;
+var cardData = [
+  { word: "한자1", meaning: "뜻1" },
+  { word: "한자2", meaning: "뜻2" },
+];
+
+function updateCard() {
+  var currentCard = cardData[currentCardIndex];
+  document.getElementById("word").innerText = currentCard.word;
+  document.getElementById("meaning").innerText = currentCard.meaning;
+}
+
+function flipCard() {
+  var cardInner = document.querySelector(".card-inner");
+  cardInner.style.transform =
+    cardInner.style.transform === "rotateY(180deg)"
+      ? "rotateY(0deg)"
+      : "rotateY(180deg)";
+}
+
+function nextCard() {
+  currentCardIndex = (currentCardIndex + 1) % cardData.length;
+  updateCard();
+  var cardInner = document.querySelector(".card-inner");
+  cardInner.style.transform = "rotateY(0deg)";
+}
+
+function prevCard() {
+  currentCardIndex = (currentCardIndex - 1 + cardData.length) % cardData.length;
+  updateCard();
+  var cardInner = document.querySelector(".card-inner");
+  cardInner.style.transform = "rotateY(0deg)";
+}
+
+updateCard();
